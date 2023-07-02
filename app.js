bf75d50b8168a298f64bb45166132157ccf751b6
skdding3 <skdding3@gmail.com>
2023-07-03 07:54:36 +0900

    HackerNews 0703 (basic)

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const ajax = new XMLHttpRequest()
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 
-ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false)
+ajax.open('GET', NEWS_URL, false)
 ajax.send()
 
 // JSON 파싱 -> 객체
 const newsFeed = JSON.parse(ajax.response)
+const ul = document.createElement('ul')
 
+// 10번 반복
+for(let i = 0; i < 10; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = newsFeed[i].title;
 
-// innerHTML
-document.getElementById('root').innerHTML = `
-<ul>
-    <li>${newsFeed[0].title}</li>
-    <li>${newsFeed[1].title}</li>
-    <li>${newsFeed[2].title}</li>
-</ul>
-`
+    ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
+
