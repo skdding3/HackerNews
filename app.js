359d8de78b5643e22ef5ac1585ddef9b169c8b39
skdding3 <skdding3@gmail.com>
2023-07-03 07:43:13 +0900

    HackerNews 0703

--- app.js
+++ app.js
@@ -1,48 +1,17 @@
-const container = document.getElementById('root');
 const ajax = new XMLHttpRequest()
-const content = document.createElement('div');
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
-// ajax 통신
-function getData(url) {
-    ajax.open('GET', NEWS_URL , false);
-    ajax.send();
-}
+ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false)
+ajax.send()
 
-// 데이터 파싱
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement('ul');
-
-window.addEventListener('hashchange', function () {
-    const id = location.hash.substr(1);
-
-    const newsContent = getData(CONTENT_URL.replace('@id', id))
-    const title = document.createElement('h1');
-
-    title.innerHTML = newsContent.title;
-
-    content.appendChild(title);
-    // console.log(newsContent)
-
-})
-
-for (let i = 0; i < 10; i++) {
-    const div = document.createElement('div')
-
-    div.innerHTML = `
-        <li>
-            <a href="#${newsFeed[i].id}">
-                ${newsFeed[i].title} (${newsFeed[i].comments_count})
-            </a>
-        </li>
-    `;
-
-    ul.appendChild(div.firstElementChild);
-}
-
-container.appendChild(ul);
-container.appendChild(content);
+// JSON 파싱 -> 객체
+const newsFeed = JSON.parse(ajax.response)
 
 
-
+// innerHTML
+document.getElementById('root').innerHTML = `
+<ul>
+    <li>${newsFeed[0].title}</li>
+    <li>${newsFeed[1].title}</li>
+    <li>${newsFeed[2].title}</li>
+</ul>
+`
