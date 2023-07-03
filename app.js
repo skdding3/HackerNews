d8b50a6fe48f0efa931adac47b6ade0d5a9fae87
skdding3 <skdding3@gmail.com>
2023-07-04 08:47:01 +0900

    HackerNews 0704 (State)

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const ajax = new XMLHttpRequest()
 const content = document.createElement('div')
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
+// STATE
 const store = {
     currentPage : 1,
+    feeds: [],
+
 };
 
 function getData(url) {
@@ -14,8 +18,16 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
+function makeFeeds(feeds) {
+    for (let  i = 0; i < feeds.length ; i++) {
+        feeds[i].read = false;
+    }
+
+    return feeds;
+}
+
 function newsFeed() {
-    const newsFeed = getData(NEWS_URL);
+    let newsFeed = store.feeds;
     const newsList = [];
     let template = `
     <div class="bg-gray-600 min-h-screen">
@@ -42,9 +54,13 @@ function newsFeed() {
     </div>
   `;
 
+    if (newsFeed.length === 0) {
+        newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    }
+
     for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
-      <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+      <div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
         <div class="flex">
           <div class="flex-auto">
             <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
@@ -63,6 +79,8 @@ function newsFeed() {
       </div>    
     `);
     }
+
+
 
     template = template.replace('{{__news_feed__}}', newsList.join(''));
     template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
@@ -109,6 +127,13 @@ function newsDetail() {
             </div>
         </div>
             `;
+
+    for (let i = 0; i < store.feeds.length; i++) {
+        if (store.feeds[i].id === Number(id)) {
+            store.feeds[i].read = true;
+            break;
+        }
+    }
 
     function makeComment(comments, called = 0) {
         const commentString = [];
