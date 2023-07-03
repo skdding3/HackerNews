2d64eaffe699cae8956eb65b68d83db09749e6fa
skdding3 <skdding3@gmail.com>
2023-07-03 14:53:37 +0900

    HackerNews 0703 (router)

--- app.js
+++ app.js
@@ -12,41 +12,66 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-// JSON 파싱 -> 객체
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul')
+function newsFeed () {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-// 해시값 변경
-window.addEventListener('hashchange', function () {
-
-    // hash.id
-    const id = location.hash.substr(1)
-
-    const newsContent = getData(CONTENT_URL.replace('@id', id), false);
-    const title = document.createElement('h1');
-
-    title.innerHTML = newsContent.title;
-
-    content.appendChild(title)
-})
+    newsList.push('<ul>');
 
 // 10번 반복
-for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 10; i++) {
 
-    // DOM API DELETE
-    const div = document.createElement('div');
+        // DOM API DELETE
 
-    div.innerHTML = `
+        newsList.push(`
     <li>
         <a href="#${newsFeed[i].id}">
             ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
     </li>
-    `
+    `)
+    }
 
-    ul.appendChild(div.firstElementChild);
+    newsList.push('</ul>');
+    container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+// JSON 파싱 -> 객체
+const ul = document.createElement('ul')
+
+
+function newsDetail() {
+
+    // hash.id
+    const id = location.hash.substr(1)
+    const newsContent = getData(CONTENT_URL.replace('@id', id), false);
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+        
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+
+    // title.innerHTML = newsContent.title;
+
+    // content.appendChild(title)
+}
+
+// Router
+function router() {
+    const routePath = location.hash;
+
+    if (routePath === '') {
+        newsFeed()
+    } else {
+        newsDetail();
+    }
+}
+
+// 해시값 변경
+window.addEventListener('hashchange', router)
+
+router()
 
